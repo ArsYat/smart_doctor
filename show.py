5e5a8583ded624b22985845dc4515e1d06a78325
yatskar <ars_yat@mail.ru>
2018-11-11 21:24:33 +0400

    show (not tested)

--- show.py
+++ show.py
@@ -3,35 +3,30 @@ import numpy as np
 from load_data import IMG_SIZE, create_train_data, load_data, TEST_DIR, TEST_FIL
 from load_model import load_model
 
+labels = ['vir', 'bac', 'nor']
 
 model = load_model()
 
 test_data = create_train_data(TEST_DIR, TEST_FIL)
 # test_data = load_data(TEST_FIL)
 
-
 fig = plt.figure()
 
 for num, data in enumerate(test_data[100:112]):
-    # cat: [1,0]
-    # dog: [0,1]
 
-    img_num = data[1]
     img_data = data[0]
 
     y = fig.add_subplot(3, 4, num + 1)
     orig = img_data
     data = img_data.reshape(IMG_SIZE, IMG_SIZE, 1)
-    # model_out = model.predict([data])[0]
     model_out = model.predict([data])[0]
 
-    if np.argmax(model_out) == 1:
-        str_label = 'vir'
-    else:
-        str_label = 'bac'
+    pred = np.argmax(model_out)
+    fact = np.argmax(data[1])
 
     y.imshow(orig)
-    plt.title(str_label)
+    plt.title('Pred:{} Fact:{}'.format(labels[pred], labels[fact]))
     y.axes.get_xaxis().set_visible(False)
     y.axes.get_yaxis().set_visible(False)
+
 plt.show()
